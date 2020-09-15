f44ea9da7efe99c557569fde963ffd8ca41b6ae9
Yuki Matsuo <yukimaterrace@gmail.com>
2020-09-15 15:16:55 +0900

    implement grid search factory

--- factory/gridsearch.go
+++ factory/gridsearch.go
@@ -1,6 +1,7 @@
 package factory
 
 import (
+	"time"
 	"yukimaterrace/andaman/broker"
 	"yukimaterrace/andaman/flow"
 	"yukimaterrace/andaman/gridsearch"
@@ -8,7 +9,16 @@ import (
 
 // CreateGridSearchFlow is a factory method to create grid search flow
 func CreateGridSearchFlow() *flow.Flow {
-	paramsForGridSearch := gridsearch.ParamsForGridSearch(flow.FrameTradeParam{})
+	paramsForGridSearch := gridsearch.FrameTradeParamsForGridSearch()
+
+	timezones := []*flow.TradableTimeZone{
+		flow.CreateTokyoAMTimeZone(),
+		flow.CreateTokyoPMTimeZone(),
+		flow.CreateLondonAMTimeZone(),
+		flow.CreateLondonPMTimeZone(),
+		flow.CreateNewYorkAMTimeZone(),
+		flow.CreateNewYorkPMTimeZone(),
+	}
 
 	pricerTradePairs := []broker.TradePair{
 		broker.GbpUsd,
@@ -23,12 +33,33 @@ func CreateGridSearchFlow() *flow.Flow {
 		broker.EurJpy,
 	}
 
-	tokyoAM := flow.CreateTokyoAMTimeZone()
-	tokyoPM := flow.CreateTokyoPMTimeZone()
-	londonAM := flow.CreateLondonAMTimeZone()
-	londonPM := flow.CreateLondonPMTimeZone()
-	newyorkAM := flow.CreateNewYorkAMTimeZone()
-	newyorkPM := flow.CreateNewYorkPMTimeZone()
+	tradeBuilder := flow.NewSimpleTraderBuilder()
+	for i, timezone := range timezones {
+		for j, param := range paramsForGridSearch {
+			algorithm := flow.NewFrameTradeAlgorithm(&param)
+			paritionID := flow.PartitionID(j + i*len(paramsForGridSearch))
 
-	return nil
+			tradeBuilder.TradableTimeZone(paritionID, timezone)
+			for _, tradePair := range pricerTradePairs {
+				tradeBuilder.Trade(paritionID, tradePair, algorithm)
+			}
+		}
+	}
+
+	tradeBuilder.Parallel(2)
+
+	start := time.Date(2020, time.July, 1, 0, 0, 0, 0, time.Local)
+	end := time.Date(2020, time.August, 31, 23, 59, 59, 0, time.Local)
+
+	flow := flow.NewFlowBuilder().
+		Broker(broker.NewSimpleSimulationBroker()).
+		OrdererFactory(broker.NewSimpleSimulationOrdererFactory()).
+		PricerTradePairs(pricerTradePairs).
+		InitialTradeMode(flow.Trade).
+		PricerFactory(flow.NewOandaSimulationPricerFactory(start, end)).
+		TraderFactory(flow.NewSimpleTraderFactory(tradeBuilder)).
+		RecorderFactory(flow.NewSimpleTradePairSummaryRecorderFactory(tradeBuilder)).
+		Build()
+
+	return flow
 }
