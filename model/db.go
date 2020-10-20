4dd1ea97a30f98ac1d809756da99563859ce316b
Yuki Matsuo <yukimaterrace@gmail.com>
2020-10-20 13:55:53 +0900

    update db

--- model/db.go
+++ model/db.go
@@ -83,8 +83,7 @@ func addTradeSet(name string, _type TradeSetType, state TradeSetState) error {
 	q := "insert into trade_set (name, type, state, created_at, updated_at) values (?, ?, ?, ?, ?)"
 
 	now := time.Now().Unix()
-	_, err := db.Exec(q, name, _type, state, now, now)
-	if err != nil {
+	if _, err := db.Exec(q, name, _type, state, now, now); err != nil {
 		return err
 	}
 	return nil
@@ -94,18 +93,16 @@ func updateTradeSetByName(name string, state TradeSetState) error {
 	q := "update trade_set set state = ?, updated_at = ? where name = ?"
 
 	now := time.Now().Unix()
-	_, err := db.Exec(q, state, now, name)
-	if err != nil {
+	if _, err := db.Exec(q, state, now, name); err != nil {
 		return err
 	}
 	return nil
 }
 
-func deleteTradeSetByName(name string) error {
-	q := "delete from trade_set where name = ?"
+func deleteTradeSet(tradeSetID int) error {
+	q := "delete from trade_set where trade_set_id = ?"
 
-	_, err := db.Exec(q, name)
-	if err != nil {
+	if _, err := db.Exec(q, tradeSetID); err != nil {
 		return err
 	}
 	return nil
@@ -141,19 +138,89 @@ func addTradeAlgorithm(_type TradeAlgorithmType, param string, tradeDirection Tr
 	paramHash := getHash(param)
 
 	q := "insert into trade_algorithm (type, param_hash, param, trade_direction) values (?, ?, ?, ?)"
-	_, err := db.Exec(q, _type, paramHash, param, tradeDirection)
-	if err != nil {
+	if _, err := db.Exec(q, _type, paramHash, param, tradeDirection); err != nil {
 		return err
 	}
 	return nil
 }
 
-func deleteTradeAlgorithmByTypeAndParam(_type TradeAlgorithmType, param string) error {
-	paramHash := getHash(param)
+func deleteTradeAlgorithm(tradeAlgorithmID int) error {
+	q := "delete from trade_algorithm where trade_algorithm_id = ?"
+	if _, err := db.Exec(q, tradeAlgorithmID); err != nil {
+		return err
+	}
+	return nil
+}
 
-	q := "delete from trade_algorithm where type = ? and param_hash = ?"
-	_, err := db.Exec(q, _type, paramHash)
+func getTradeConfigurationByFields(tradePair TradePair, timezone Timezone, tradeAlgorithmID int) (*TradeConfiguration, error) {
+	q := "select * from trade_configuration where trade_pair = ? and timezone = ? and trade_algorithm_id = ?"
+
+	tc := TradeConfiguration{}
+	row := db.QueryRow(q, tradePair, timezone, tradeAlgorithmID)
+	err := row.Scan(
+		&tc.TradeConfigurationID,
+		&tc.TradePair,
+		&tc.Timezone,
+		&tc.TradeAlgorithmID,
+	)
 	if err != nil {
+		return nil, err
+	}
+
+	return &tc, nil
+}
+
+func addTradeConfiguration(tradePair TradePair, timezone Timezone, tradeAlgorithmID int) error {
+	q := "insert into trade_configuration (trade_pair, timezone, trade_algorithm_id) values (?, ?, ?)"
+
+	if _, err := db.Exec(q, tradePair, timezone, tradeAlgorithmID); err != nil {
+		return err
+	}
+	return nil
+}
+
+func deleteTradeConfiguration(tradeConfigurationID int) error {
+	q := "delete from trade_configuration where trade_configuration_id = ?"
+
+	if _, err := db.Exec(q, tradeConfigurationID); err != nil {
+		return err
+	}
+	return nil
+}
+
+func getTradeSetConfigurationRelsByTradeSetID(tradeSetID int) ([]TradeSetConfigurationRel, error) {
+	q := "select * from trade_set_configuration_rel where trade_set_id = ?"
+
+	rows, err := db.Query(q, tradeSetID)
+	if err != nil {
+		return nil, err
+	}
+
+	var rels []TradeSetConfigurationRel
+	for rows.Next() {
+		rel := TradeSetConfigurationRel{}
+		if err := rows.Scan(&rel.TradeSetID, &rel.TradeConfigurationID); err != nil {
+			return nil, err
+		}
+		rels = append(rels, rel)
+	}
+
+	return rels, nil
+}
+
+func addTradeSetConfigurationRel(tradeSetID int, tradeConfigurationID int) error {
+	q := "insert into trade_set_configuration_rel (trade_set_id, trade_configuration_id) values (?, ?)"
+
+	if _, err := db.Exec(q, tradeSetID, tradeConfigurationID); err != nil {
+		return err
+	}
+	return nil
+}
+
+func deleteTradeSetConfigurationRelByTradeSetID(tradeSetID int) error {
+	q := "delete from trade_set_configuration where trade_set_id = ?"
+
+	if _, err := db.Exec(q, tradeSetID); err != nil {
 		return err
 	}
 	return nil
