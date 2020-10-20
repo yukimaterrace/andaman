250a5175068cc11eca959fbfad12db054be0ddef
Yuki Matsuo <yukimaterrace@gmail.com>
2020-10-20 14:23:59 +0900

    add json setting

---
+++ model/definitions.go
@@ -0,0 +1,6 @@
+package model
+
+// TradeSetsResponse is a response for trade sets
+type TradeSetsResponse struct {
+	TradeSets []TradeSet `json:"trade_sets"`
+}
--- model/tables.go
+++ model/tables.go
@@ -2,29 +2,29 @@ package model
 
 // TradeSet is a struct for trade set table
 type TradeSet struct {
-	TradeSetID int
-	Name       string
-	Type       TradeSetType
-	State      TradeSetState
-	CreatedAt  int
-	UpdatedAt  int
+	TradeSetID int           `json:"-"`
+	Name       string        `json:"name"`
+	Type       TradeSetType  `json:"type"`
+	State      TradeSetState `json:"state"`
+	CreatedAt  int           `json:"-"`
+	UpdatedAt  int           `json:"updated_at"`
 }
 
 // TradeAlgorithm is a struct for trade algorithm table
 type TradeAlgorithm struct {
-	TradeAlgorithmID int
-	Type             TradeAlgorithmType
-	ParamHash        string
-	Param            string
-	TradeDirection   TradeDirection
+	TradeAlgorithmID int                `json:"-"`
+	Type             TradeAlgorithmType `json:"type"`
+	ParamHash        string             `json:"-"`
+	Param            string             `json:"param"`
+	TradeDirection   TradeDirection     `json:"trade_direction"`
 }
 
 // TradeConfiguration is a struct for trade configuration table
 type TradeConfiguration struct {
-	TradeConfigurationID int
-	TradePair            TradePair
-	Timezone             Timezone
-	TradeAlgorithmID     int
+	TradeConfigurationID int       `json:"-"`
+	TradePair            TradePair `json:"trade_pair"`
+	Timezone             Timezone  `json:"timezone"`
+	TradeAlgorithmID     int       `json:"-"`
 }
 
 // TradeSetConfigurationRel is a struct for trade set configurationrel table
@@ -35,14 +35,14 @@ type TradeSetConfigurationRel struct {
 
 // Order is a struct for order table
 type Order struct {
-	OrderID              int
-	BrokerOrderID        int
-	TradeConfigurationID int
-	Units                float64
-	State                OrderState
-	Profit               float64
-	TimeAtOpen           int
-	PriceAtOpen          float64
-	TimeAtClose          int
-	PriceAtClose         float64
+	OrderID              int        `json:"-"`
+	BrokerOrderID        int        `json:"order_id"`
+	TradeConfigurationID int        `json:"-"`
+	Units                float64    `json:"units"`
+	State                OrderState `json:"state"`
+	Profit               float64    `json:"profit"`
+	TimeAtOpen           int        `json:"time_at_open"`
+	PriceAtOpen          float64    `json:"price_at_open"`
+	TimeAtClose          int        `json:"time_at_close"`
+	PriceAtClose         float64    `json:"price_at_close"`
 }
