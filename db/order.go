55a1f410f5e63ffea071b997cf84a916033ecac0
Yuki Matsuo <yukimaterrace@gmail.com>
2020-11-03 10:45:02 +0900

    implement order db

--- db/order.go
+++ db/order.go
@@ -1,6 +1,9 @@
 package db
 
-import "yukimaterrace/andaman/model"
+import (
+	"database/sql"
+	"yukimaterrace/andaman/model"
+)
 
 // GetOrderByTradeRunAndBrokerOrder is a method to get order by trade run and broker order
 func GetOrderByTradeRunAndBrokerOrder(tradeRunID int, brokerOrderID int) (*model.Order, error) {
@@ -65,9 +68,10 @@ func GetOrdersByTradeRunAndState(tradeRunID int, state model.OrderState) ([]*mod
 	return orders, nil
 }
 
-// GetOpenOrders is a method to get open orders
-func GetOpenOrders(
-	tradeRunID int, tradePair model.TradePair, timezone model.Timezone, tradeDirection model.TradeDirection, algorithmType model.TradeAlgorithmType) ([]*model.Order, error) {
+func getOrders(
+	tradeRunID int, state model.OrderState,
+	tradePair model.TradePair, timezone model.Timezone, tradeDirection model.TradeDirection, algorithmType model.TradeAlgorithmType,
+	count int, offset int) ([]*model.Order, error) {
 	q := `
 		select
 			order_.trade_run_id,
@@ -93,15 +97,16 @@ func GetOpenOrders(
 			trade_configuration.timezone = ? and
 			trade_algorithm.type = ? and
 			trade_algorithm.trade_direction = ?
-		`
+		limit ?
+		offset ?
+	`
 
-	orders := []*model.Order{}
-
-	rows, err := db.Query(q, tradeRunID, model.Open, tradePair, timezone, algorithmType, tradeDirection)
+	rows, err := db.Query(q, tradeRunID, state, tradePair, timezone, algorithmType, tradeDirection, count, offset)
 	if err != nil {
 		return nil, err
 	}
 
+	orders := []*model.Order{}
 	for rows.Next() {
 		var order model.Order
 		err := rows.Scan(
@@ -124,6 +129,115 @@ func GetOpenOrders(
 	}
 
 	return orders, nil
+}
+
+func getTotalProfitForOrders(
+	tradeRunID int, state model.OrderState,
+	tradePair model.TradePair, timezone model.Timezone, tradeDirection model.TradeDirection, algorithmType model.TradeAlgorithmType) (float64, error) {
+	q := `
+		select
+			sum(order_.profit)
+		from
+			order_,
+			trade_configuration,
+			trade_algorithm
+		where
+			order_.trade_run_id = ? and
+			order_.state = ? and
+			order_.trade_configuration_id = trade_configuration.trade_configuration_id and
+			trade_configuration.trade_algorithm_id = trade_algorithm.trade_algorithm_id and
+			trade_configuration.trade_pair = ? and
+			trade_configuration.timezone = ? and
+			trade_algorithm.type = ? and
+			trade_algorithm.trade_direction = ?
+		`
+
+	row := db.QueryRow(q, tradeRunID, state, tradePair, timezone, algorithmType, tradeDirection)
+
+	var totalProfit float64
+	if err := row.Scan(&totalProfit); err != nil {
+		if err == sql.ErrNoRows {
+			return 0, nil
+		}
+		return 0, err
+	}
+
+	return totalProfit, nil
+}
+
+func getCountForOrders(
+	tradeRunID int, state model.OrderState,
+	tradePair model.TradePair, timezone model.Timezone, tradeDirection model.TradeDirection, algorithmType model.TradeAlgorithmType) (int, error) {
+	q := `
+		select
+			count(1)
+		from
+			order_,
+			trade_configuration,
+			trade_algorithm
+		where
+			order_.trade_run_id = ? and
+			order_.state = ? and
+			order_.trade_configuration_id = trade_configuration.trade_configuration_id and
+			trade_configuration.trade_algorithm_id = trade_algorithm.trade_algorithm_id and
+			trade_configuration.trade_pair = ? and
+			trade_configuration.timezone = ? and
+			trade_algorithm.type = ? and
+			trade_algorithm.trade_direction = ?
+		`
+
+	row := db.QueryRow(q, tradeRunID, state, tradePair, timezone, algorithmType, tradeDirection)
+
+	var count int
+	if err := row.Scan(&count); err != nil {
+		return 0, err
+	}
+
+	return count, nil
+}
+
+// GetOpenOrders is a method to get open orders
+func GetOpenOrders(
+	tradeRunID int, tradePair model.TradePair, timezone model.Timezone, tradeDirection model.TradeDirection, algorithmType model.TradeAlgorithmType,
+	count int, offset int) ([]*model.Order, error) {
+
+	return getOrders(tradeRunID, model.Open, tradePair, timezone, tradeDirection, algorithmType, count, offset)
+}
+
+// GetTotalProfitForOpenOrders is a method to get total profit for open orders
+func GetTotalProfitForOpenOrders(
+	tradeRunID int, tradePair model.TradePair, timezone model.Timezone, tradeDirection model.TradeDirection, algorithmType model.TradeAlgorithmType) (float64, error) {
+
+	return getTotalProfitForOrders(tradeRunID, model.Open, tradePair, timezone, tradeDirection, algorithmType)
+}
+
+// GetCountForOpenOrders is a method to get count for open orders
+func GetCountForOpenOrders(
+	tradeRunID int, tradePair model.TradePair, timezone model.Timezone, tradeDirection model.TradeDirection, algorithmType model.TradeAlgorithmType) (int, error) {
+
+	return getCountForOrders(tradeRunID, model.Open, tradePair, timezone, tradeDirection, algorithmType)
+}
+
+// GetClosedOrders is a method to get closed orders
+func GetClosedOrders(
+	tradeRunID int, tradePair model.TradePair, timezone model.Timezone, tradeDirection model.TradeDirection, algorithmType model.TradeAlgorithmType,
+	count int, offset int) ([]*model.Order, error) {
+
+	return getOrders(tradeRunID, model.Closed, tradePair, timezone, tradeDirection, algorithmType, count, offset)
+}
+
+// GetTotalProfitForClosedOrders is a method to get total profit for closed orders
+func GetTotalProfitForClosedOrders(
+	tradeRunID int, tradePair model.TradePair, timezone model.Timezone, tradeDirection model.TradeDirection, algorithmType model.TradeAlgorithmType) (float64, error) {
+
+	return getTotalProfitForOrders(tradeRunID, model.Closed, tradePair, timezone, tradeDirection, algorithmType)
+}
+
+// GetCountForClosedOrders is a method to get count for closed orders
+func GetCountForClosedOrders(
+	tradeRunID int, tradePair model.TradePair, timezone model.Timezone, tradeDirection model.TradeDirection, algorithmType model.TradeAlgorithmType) (int, error) {
+
+	return getCountForOrders(tradeRunID, model.Closed, tradePair, timezone, tradeDirection, algorithmType)
 }
 
 // AddOrder is a method to add order
